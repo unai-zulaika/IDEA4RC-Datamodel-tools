14f1c70b938c9b45f227b20a517b01ca5ec6990e
Unai Zulaika <unai.zulaika@deusto.es>
2023-11-03 14:13:28 +0100

    updated code

--- FHIR_format/script.py
+++ FHIR_format/script.py
@@ -1,36 +1,41 @@
 import pandas as pd
 from tqdm import tqdm
 
-INPUT_FILEPATH = 'IDEA4RC_DM_H&N_V1.xlsx'
+SHEET_ID = "1Vw1Dr2K4oG__cDQTutGaJhZvGUvQTLwc4qWreP6qMSs"
 
-INPUT_FILEPATH = 'IDEA4RC_DM_V1.xlsx'
-OUTPUT_FILEPATH = 'output.xlsx'
+# INPUT_FILEPATH = 'IDEA4RC_DM_V1.xlsx'
+OUTPUT_FILEPATH = 'output_fhir.xlsx'
 
-DROP_COLUMNS = ['Variable Name (EURACAN file)','DataElementConcept        ', 'ObjectClass', 'ConventionEN', 'Example', 'Variable as modifier','Value in OMOP', 'Notes']
-COLUMNS_RENAME_MAP = {'ObjectProperty	': 'Element name',
-                      'ObjectPropertyLabelEN        ': 'DataElement', 
+DROP_COLUMNS = ['Variable Name (EURACAN file)','DataElementConcept', 'ObjectClass', 'ConventionEN', 'Example', 'Variable as modifier','Value in OMOP', 'Notes',]
+COLUMNS_RENAME_MAP = {'ObjectProperty': 'Element name',
+                      'ObjectPropertyLabelEN': 'DataElement', 
                       'DataElementConceptDefEN': 'Data element description',
-                      'FormatConceptualDomain        ': 'dataType',
+                      'FormatConceptualDomain': 'dataType',
                       'index': 'Description'}
 COLUMNS_REORDER = ['Element name', 'DataElement', 'Data element description', 'min', 'max', 'dataType', 'Comment']
 
-SHEETS_TO_PROCESS = list(range(2, 16))
+SHEETS_TO_PROCESS = list(range(3, 19))
 
 print('Starting conversion...')
 
 # load file
-xls = pd.ExcelFile(INPUT_FILEPATH)
+xls = pd.ExcelFile(
+    f"https://docs.google.com/spreadsheets/export?id={SHEET_ID}&format=xlsx"
+)
 
 #let's process each sheet
-with pd.ExcelWriter(OUTPUT_FILEPATH) as writer:
+with open(OUTPUT_FILEPATH, "wb") as f:
     for index, sheet_number in enumerate(tqdm(SHEETS_TO_PROCESS)):
         # read each sheet
         dataframe = pd.read_excel(xls, sheet_name=sheet_number)
-
         # drop undesired columns
         dataframe = dataframe.drop(columns=DROP_COLUMNS)
+        print(dataframe.columns)
+        
         # rename columns
         dataframe = dataframe.rename(columns=COLUMNS_RENAME_MAP)
+        print(dataframe.columns)
+
         # clean empty rows
         dataframe = dataframe[dataframe['Element name'].notna()].reset_index()
 
@@ -57,6 +62,8 @@ with pd.ExcelWriter(OUTPUT_FILEPATH) as writer:
         dataframe['dataType'] = dataframe['dataType'].str.replace('code','CodeableConcept')
         dataframe['dataType'] = dataframe['dataType'].str.replace('customCode','CodeableConcept')
 
+        print(dataframe.columns)
+    
         # reorder columns
         dataframe = dataframe[COLUMNS_REORDER]
 
@@ -64,4 +71,5 @@ with pd.ExcelWriter(OUTPUT_FILEPATH) as writer:
         dataframe.index.name='Description'
 
         # after conversion write output per sheet
-        dataframe.to_excel(writer, sheet_name=xls.sheet_names[sheet_number], index=True)
+        dataframe.to_excel(f, sheet_name=xls.sheet_names[sheet_number], index=True)
+
